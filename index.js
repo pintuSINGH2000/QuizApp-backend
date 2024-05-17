d24c3e16c2fef7f2815bcc1c1659583feb366fe4
pintuSINGH2000 <94526600+pintuSINGH2000@users.noreply.github.com>
2024-05-17 17:12:50 +0530

    database configuration

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
 const app = express();
+
+dotenv.config();
+connectDB();
 
 app.get("/",(req,res)=>{
     res.send("Hello World");
